90cc59d1f1cbb18ed88dba164c5b614766922eff
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2022-02-11 01:55:45 +0000

    New Dockerfile.

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    // Used as fallback if no runtime config is provided
     baseURL: 'https://localhost'
   },
 
